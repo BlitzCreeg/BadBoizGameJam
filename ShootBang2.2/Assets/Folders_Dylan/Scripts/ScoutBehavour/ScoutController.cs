baf4d05a7b934534cfef538b169f0397800ebadb
Dylan-Gilchrist <32092289+Dylan-Gilchrist@users.noreply.github.com>
2021-02-26 17:15:04 +0000

    AI Shit

---
+++ ShootBang2.2/Assets/Folders_Dylan/Scripts/ScoutBehavour/ScoutController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class ScoutController : MonoBehaviour
+{
+    public NavMeshAgent scoutAI;
+    public GameObject player;
+    public bool isScouting;
+    GameObject[] allSquads;
+    public GameObject squadClosest;
+    public GameObject moveTarget;
+    public MoveScout moveScout;
+    public Vector3 playerFoundPosition;
+
+    // public static object Instance { get; internal set; }
+
+    void Start()
+    {
+        GetAllSquads();
+        scoutAI = GetComponent<NavMeshAgent>();
+        moveScout = moveTarget.GetComponent<MoveScout>();
+        isScouting = true;
+    }
+
+    void Update()
+    {
+        Scouting();
+
+        if (isScouting)
+        {
+            scoutAI.SetDestination(moveTarget.transform.position);
+            moveScout.canMove = isScouting;
+        }
+
+        // Finding Closest Squad
+        if (!isScouting)
+        {
+            squadClosest = GetClosestSquad(allSquads);
+
+            // Scout Travels To Closest Squad
+            AlertSquad();
+        }
+    }
+
+    void GetAllSquads()
+    {
+        allSquads = GameObject.FindGameObjectsWithTag("Squad");
+    }
+
+    GameObject GetClosestSquad (GameObject[] squads)
+    {
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+        foreach(GameObject potentialTarget in squads)
+        {
+            Vector3 directionToTarget = potentialTarget.transform.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if(dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget.transform;
+            }
+        }
+     
+        return bestTarget.gameObject;
+    }
+
+    void Scouting()
+    {
+        float distBetweenPlayerScout = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+        if (distBetweenPlayerScout < 2)
+        {
+            // Set Player Position To Last Known Position
+            playerFoundPosition = player.transform.position;
+
+            if (squadClosest == null)
+            {
+                squadClosest = GetClosestSquad(allSquads);
+            }
+
+            isScouting = false;
+            GetClosestSquad(allSquads);
+        }
+    }
+
+    void AlertSquad()
+    {
+        scoutAI.SetDestination(squadClosest.GetComponent<Transform>().transform.position);
+    }
+}
---
+++ ShootBang2.2/Assets/Folders_Dylan/Scripts/ScoutBehavour/WaypointNodesController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointNodesController : MonoBehaviour {
+
+    public static Transform[] waypointNodes;
+
+    void Awake()
+    {
+        waypointNodes = new Transform[transform.childCount];
+        for (int i = 0; i < waypointNodes.Length; i++)
+        {
+            waypointNodes[i] = transform.GetChild(i);
+        }
+    }
+}
