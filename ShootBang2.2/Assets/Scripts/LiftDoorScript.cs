c0a1f1c89b6412d80dd8bdc5574c7b54ab9fcb15
Stefan Lewis <11997427+Stefan-Lewis@users.noreply.github.com>
2021-02-27 23:02:28 +0000

    Doooooooooors

---
+++ ShootBang2.2/Assets/Scripts/LiftDoorScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftDoorScript : MonoBehaviour
+{
+    public Animator anim;
+    public AudioSource aud;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.Play("LiftDoorsOpenClose");
+            aud.Play();
+        }
+    }
+}
