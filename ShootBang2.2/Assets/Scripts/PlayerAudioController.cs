fae304cfdbe2a31923a908f09197e2f3ccd9b6ef
snapdougles <danzyrules@yahoo.co.uk>
2021-02-28 21:11:16 +0000

    added sound impact implementation, refined sfx implementation

--- ShootBang2.2/Assets/Scripts/PlayerAudioController.cs
+++ ShootBang2.2/Assets/Scripts/PlayerAudioController.cs
@@ -5,14 +5,47 @@ using UnityEngine;
 public class PlayerAudioController : MonoBehaviour
 {
     public AudioClip[] concreteCrouchArray;
+    public AudioClip[] metalCrouchArray;
     public AudioClip[] concreteWalkArray;
+    public AudioClip[] metalWalkArray;
     public AudioClip[] concreteSprintArray;
-    public AudioClip[] jumpArray;
-    public AudioSource effectSource;
+    public AudioClip[] metalSprintArray;
+
+    public AudioClip[] pistolHitArray;
+    public AudioClip[] pistolChargeArray;
+    public AudioClip pistolShot;
+    public AudioClip pistolChirp;
+    public AudioClip pistolChirp2;
+    public AudioClip pistolWait;
+
+    public AudioClip[] bulletHitArray;
+    public AudioClip[] concreteMissArray;
+
+    public AudioClip[] handlingArray;
+
+    public AudioClip jump;
+
+    public AudioSource playerSource;
+    public AudioSource pistolSource;
+    public AudioSource pistolWSSource;
+    public AudioSource handlingSource;
+
     public PlayerMovement playerMovement;
+    public Pistol pistol;
+
+    public string material;
+    int lastBeep;
     float pitchMin, pitchMax, volumeMin, volumeMax;
     private int clipIndex = 0;
     int i;
+    bool waiting;
+    bool falling = false;
+    float velocity = 0.0f;
+
+    private void Start()
+    {
+        material = "concrete";
+    }
 
     void PlayRandomConcreteCrouch()
     {
@@ -20,45 +53,78 @@ public class PlayerAudioController : MonoBehaviour
         pitchMax = 1.1f;
         volumeMin = 0.6f;
         volumeMax = .8f;
-        effectSource.pitch = Random.Range(pitchMin, pitchMax);
-        effectSource.volume = Random.Range(volumeMin, volumeMax);
+        playerSource.pitch = Random.Range(pitchMin, pitchMax);
+        playerSource.volume = Random.Range(volumeMin, volumeMax);
         clipIndex = RepeatCheck(clipIndex, concreteCrouchArray.Length);
-        effectSource.PlayOneShot(concreteCrouchArray[clipIndex]);
+        playerSource.PlayOneShot(concreteCrouchArray[clipIndex]);
     }
-
+    void PlayRandomMetalCrouch()
+    {
+        pitchMin = 0.9f;
+        pitchMax = 1.1f;
+        volumeMin = 0.6f;
+        volumeMax = .8f;
+        playerSource.pitch = Random.Range(pitchMin, pitchMax);
+        playerSource.volume = Random.Range(volumeMin, volumeMax);
+        clipIndex = RepeatCheck(clipIndex, metalCrouchArray.Length);
+        playerSource.PlayOneShot(metalCrouchArray[clipIndex]);
+    }
     void PlayRandomConcreteWalk()
     {
         pitchMin = 0.8f;
         pitchMax = 1f;
         volumeMin = 0.25f;
         volumeMax = 0.5f;
-        effectSource.pitch = Random.Range(pitchMin, pitchMax);
-        effectSource.volume = Random.Range(volumeMin, volumeMax);
+        playerSource.pitch = Random.Range(pitchMin, pitchMax);
+        playerSource.volume = Random.Range(volumeMin, volumeMax);
         clipIndex = RepeatCheck(clipIndex, concreteWalkArray.Length);
-        effectSource.PlayOneShot(concreteWalkArray[clipIndex]);
+        playerSource.PlayOneShot(concreteWalkArray[clipIndex]);
     }
-
+    void PlayRandomMetalWalk()
+    {
+        pitchMin = 0.8f;
+        pitchMax = 1f;
+        volumeMin = 0.25f;
+        volumeMax = 0.5f;
+        playerSource.pitch = Random.Range(pitchMin, pitchMax);
+        playerSource.volume = Random.Range(volumeMin, volumeMax);
+        clipIndex = RepeatCheck(clipIndex, metalWalkArray.Length);
+        playerSource.PlayOneShot(metalWalkArray[clipIndex]);
+    }
     void PlayRandomConcreteSprint()
     {
         pitchMin = 0.9f;
         pitchMax = 1.2f;
         volumeMin = 0.8f;
         volumeMax = 1.4f;
-        effectSource.pitch = Random.Range(pitchMin, pitchMax);
-        effectSource.volume = Random.Range(volumeMin, volumeMax);
+        playerSource.pitch = Random.Range(pitchMin, pitchMax);
+        playerSource.volume = Random.Range(volumeMin, volumeMax);
         clipIndex = RepeatCheck(clipIndex, concreteSprintArray.Length);
-        effectSource.PlayOneShot(concreteSprintArray[clipIndex]);
+        playerSource.PlayOneShot(concreteSprintArray[clipIndex]);
     }
-
-    void PlayRandomJump()
+    void PlayRandomMetalSprint()
+    {
+        pitchMin = 0.9f;
+        pitchMax = 1.2f;
+        volumeMin = 0.8f;
+        volumeMax = 1.4f;
+        playerSource.pitch = Random.Range(pitchMin, pitchMax);
+        playerSource.volume = Random.Range(volumeMin, volumeMax);
+        clipIndex = RepeatCheck(clipIndex, metalSprintArray.Length);
+        playerSource.PlayOneShot(metalSprintArray[clipIndex]);
+    }
+    void PlayRandomHandling()
     {
         pitchMin = 0.8f;
         pitchMax = 1.2f;
         volumeMin = 0.8f;
         volumeMax = 1.2f;
-        clipIndex = RepeatCheck(clipIndex, jumpArray.Length);
-        effectSource.PlayOneShot(jumpArray[clipIndex]);
+        handlingSource.pitch = Random.Range(pitchMin, pitchMax);
+        handlingSource.volume = Random.Range(volumeMin, volumeMax);
+        clipIndex = RepeatCheck(clipIndex, handlingArray.Length);
+        handlingSource.PlayOneShot(handlingArray[clipIndex]);
     }
+
 
     int RepeatCheck(int previousIndex, int range)
     {
@@ -71,26 +137,161 @@ public class PlayerAudioController : MonoBehaviour
         return index;
     }
 
+    public void ChargeBeep()
+    {
+        if (pistol.charge < 10)
+        {
+            pistolSource.volume = 0.005f;
+            pistolSource.PlayOneShot(pistolChargeArray[0]);
+            lastBeep = 0;
+        }
+        else if (pistol.charge >= 10 && pistol.charge < 20)
+        {
+            if (lastBeep == 0)
+            {
+                pistolSource.volume = 0.02f;
+            }
+            else
+            {
+                pistolSource.volume = 0.005f;
+            }
+            pistolSource.PlayOneShot(pistolChargeArray[1]);
+            lastBeep = 1;
+        }
+        else if (pistol.charge >= 20 && pistol.charge < 30)
+        {
+            if (lastBeep == 1)
+            {
+                pistolSource.volume = 0.02f;
+            }
+            else
+            {
+                pistolSource.volume = 0.005f;
+            }
+            pistolSource.PlayOneShot(pistolChargeArray[2]);
+            lastBeep = 2;
+        }
+        else if (pistol.charge >= 30 && pistol.charge < 40)
+        {
+            if (lastBeep == 2)
+            {
+                pistolSource.volume = 0.02f;
+            }
+            else
+            {
+                pistolSource.volume = 0.005f;
+            }
+            pistolSource.PlayOneShot(pistolChargeArray[3]);
+            lastBeep = 3;
+        }
+        else if (pistol.charge >= 40 && pistol.charge < 50)
+        {
+            if (lastBeep == 3)
+            {
+                pistolSource.volume = 0.02f;
+            }
+            else
+            {
+                pistolSource.volume = 0.005f;
+            }
+            pistolSource.PlayOneShot(pistolChargeArray[4]);
+            lastBeep = 4;
+        }
+        else if (pistol.charge == 50)
+        {
+            if (lastBeep == 4)
+            {
+                pistolSource.volume = 0.02f;
+                pistolSource.PlayOneShot(pistolChirp);
+            }
+            else
+            {
+                pistolSource.volume = 0.005f;
+            }
+            pistolSource.PlayOneShot(pistolChargeArray[5]);
+            lastBeep = 5;
+        }
+    }
+    public void PistolWait()
+    {
+        if (waiting == true)
+        {
+            pistolWSSource.Stop();
+        }
+        waiting = true;
+        pistolWSSource.pitch = 1.0f;
+        pistolWSSource.volume = 0.25f;
+        pistolWSSource.PlayOneShot(pistolWait);
+    }
+
+    public void PistolShot()
+    {
+        if (pistol.charge <10)
+        {
+            pistolWSSource.volume = 0.2f;
+            pistolWSSource.pitch = 1.0f;
+            pistolWSSource.PlayOneShot(pistolChirp2);
+
+        }
+        else
+        {
+            pitchMin = 0.9f;
+            pitchMax = 1.2f;
+            volumeMin = 0.6f;
+            volumeMax = 0.9f;
+            pistolWSSource.pitch = Random.Range(pitchMin, pitchMax);
+            pistolWSSource.volume = Random.Range(volumeMin, volumeMax);
+            pistolWSSource.PlayOneShot(pistolShot);
+            waiting = false;
+        }
+    }
+
     void FixedUpdate()
     {
+
+        if (playerMovement.isJumping == true)
+        {
+            print("ganabbabta");
+            pitchMin = 0.8f;
+            pitchMax = 1.2f;
+            volumeMin = 0.2f;
+            volumeMax = 0.5f;
+            playerSource.pitch = Random.Range(pitchMin, pitchMax);
+            playerSource.volume = Random.Range(volumeMin, volumeMax);
+            playerSource.PlayOneShot(jump);
+        }
+
         if (playerMovement.isWalking == true)
         {
             if (i >= 35)
             {
-                PlayRandomConcreteWalk();
+                if (material == "concrete")
+                {
+                    PlayRandomConcreteWalk();
+                }
+                else if (material == "metal")
+                {
+                    PlayRandomMetalWalk();
+                }
                 i = 0;
             }
             else
             {
                 i = i + 1;
             }
-            print("walking");
         }
         else if (playerMovement.isCrouchWalking == true)
         {
             if (i >= 50)
             {
-                PlayRandomConcreteCrouch();
+                if (material == "concrete")
+                {
+                    PlayRandomConcreteCrouch();
+                }
+                else if (material == "metal")
+                {
+                    PlayRandomMetalCrouch();
+                }
                 i = 0;
 
             }
@@ -98,12 +299,20 @@ public class PlayerAudioController : MonoBehaviour
             {
                 i = i + 1;
             }
+
         }
         else if (playerMovement.isSprinting == true)
         {
             if (i >= 15)
             {
-                PlayRandomConcreteSprint();
+                if (material == "concrete")
+                {
+                    PlayRandomConcreteSprint();
+                }
+                else if (material == "metal")
+                {
+                    PlayRandomMetalSprint();
+                }
                 i = 0;
             }
             else
@@ -112,22 +321,28 @@ public class PlayerAudioController : MonoBehaviour
             }
         }
 
-        if (playerMovement.isJumping == true)
+        if (playerMovement.isGrounded == false)
         {
-            PlayRandomJump();
+            falling = true;
+            velocity = playerMovement.currentYVelocity;
         }
-
-        if (Input.GetKeyDown("j"))
+        else if (playerMovement.isGrounded == true && falling == true)
         {
-            PlayRandomConcreteCrouch();
-        }
-        if (Input.GetKeyDown("k"))
-        {
-            PlayRandomConcreteWalk();
-        }
-        if (Input.GetKeyDown("l"))
-        {
-            PlayRandomConcreteSprint();
+            falling = false;
+            pitchMin = 0.8f;
+            pitchMax = 1.2f;
+            playerSource.pitch = Random.Range(pitchMin, pitchMax);
+            playerSource.volume = -0.01f*velocity;
+            if (material == "concrete")
+            {
+                clipIndex = RepeatCheck(clipIndex, concreteSprintArray.Length);
+                playerSource.PlayOneShot(concreteSprintArray[clipIndex]);
+            }
+            else if (material == "metal")
+            {
+                clipIndex = RepeatCheck(clipIndex, metalSprintArray.Length);
+                playerSource.PlayOneShot(metalSprintArray[clipIndex]);
+            }
         }
     }
 }
