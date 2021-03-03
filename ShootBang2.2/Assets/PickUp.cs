9f3e1c23125272dc487216e65517d41a93b56383
Dylan-Gilchrist <32092289+Dylan-Gilchrist@users.noreply.github.com>
2021-03-03 17:59:27 +0000

    Nav Mesh

---
+++ ShootBang2.2/Assets/PickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(1 , 1, 0), Space.World);
+    }
+}
